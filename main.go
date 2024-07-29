ce2bd153a0c9f91578c480be3f38c35ff6793f50
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2024-07-29 14:15:27 -0300

    Awful basic ass subcommand scaffold

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/kaiserofnone/dmv/bot"
 
@@ -52,6 +54,89 @@ func PongHandler(
 	})
 }
 
+type UserConfig struct {
+	Timezone *time.Location
+}
+
+type UserConfigManager struct {
+	logger      *log.Logger
+	UserConfigs map[string]UserConfig
+}
+
+func LoadUserConfigs(logger *log.Logger) UserConfigManager {
+	return UserConfigManager{
+		logger:      logger,
+		UserConfigs: map[string]UserConfig{},
+	}
+}
+
+var ConfigureDescriptor = &discord.ApplicationCommand{
+	Name:        "configure",
+	Description: "Configure User settings",
+	Options: []*discord.ApplicationCommandOption{
+		{
+			Name:        "timezone",
+			Description: "Timezone related commands",
+			Type:        discord.ApplicationCommandOptionSubCommandGroup,
+			Options: []*discord.ApplicationCommandOption{
+				{
+					Name:        "set",
+					Description: "Sets the user timezone, use IANA timezone format (continent/city)",
+					Type:        discord.ApplicationCommandOptionSubCommand,
+					Options: []*discord.ApplicationCommandOption{
+						{
+							Name:        "timezone",
+							Description: "IANA timezone name",
+							Type:        discord.ApplicationCommandOptionString,
+							Required:    true,
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+func (uc *UserConfigManager) ConfigureHandler(
+	s *discord.Session,
+	i *discord.InteractionCreate,
+	data discord.ApplicationCommandInteractionData) {
+	if data.Options[0].Name == "timezone" {
+		uc.ConfigureTimezoneHandler(s, i, data.Options[0].Options[0])
+	}
+}
+
+func (uc *UserConfigManager) ConfigureTimezoneHandler(
+	s *discord.Session,
+	i *discord.InteractionCreate,
+	data *discord.ApplicationCommandInteractionDataOption) {
+	user := i.Interaction.Member.User.ID
+	if data.Name == "set" {
+		tzname := (data.Options[0].Value).(string)
+		location, err := time.LoadLocation(tzname)
+		if err != nil {
+			s.InteractionRespond(i.Interaction, &discord.InteractionResponse{
+				Type: discord.InteractionResponseChannelMessageWithSource,
+				Data: &discord.InteractionResponseData{
+					Content: fmt.Sprintf("Invalid location: %s: %v", location, err),
+					Flags:   discord.MessageFlagsEphemeral,
+				},
+			})
+			return
+		}
+		userConfig := uc.UserConfigs[user]
+		userConfig.Timezone = location
+		uc.UserConfigs[user] = userConfig
+		s.InteractionRespond(i.Interaction, &discord.InteractionResponse{
+			Type: discord.InteractionResponseChannelMessageWithSource,
+			Data: &discord.InteractionResponseData{
+				Content: fmt.Sprintf("Timezone set to %s", location.String()),
+				Flags:   discord.MessageFlagsEphemeral,
+			},
+		})
+	}
+}
+
 func main() {
 	logf, err := os.Create("bot.log")
 	if err != nil {
@@ -66,8 +151,10 @@ func main() {
 	if err != nil {
 		logger.Fatalf("Failed to load config: %v", err)
 	}
+	userConfigs := LoadUserConfigs(logger)
 	discordBot, err := bot.NewBot(cfg.BotConfig, logger)
 	discordBot.RegisterHandler(PongDescriptor, PongHandler)
+	discordBot.RegisterHandler(ConfigureDescriptor, userConfigs.ConfigureHandler)
 	go func() {
 		err = discordBot.Start()
 		if err != nil {
